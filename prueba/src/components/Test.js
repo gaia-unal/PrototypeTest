192d8f88e5c304a8fbb5898b4e780867b8c75c5f
MariaJoseGG <mgiraldogu@unal.edu.co>
2023-09-25 19:23:12 -0500

    The iframe is organized so that it is responsive and does not hide the content of the application

--- prueba/src/components/Test.js
+++ prueba/src/components/Test.js
@@ -6,11 +6,15 @@ function Test() {
     // This is the domain to get the activities' route
     const domain = "http://localhost:3000";
 
-    const srcIframe = domain + "/Activities/EarlyDetection/PhonologicalDyslexia/PhonologicalAwareness/1/indice.html";
+    const srcIframe = [
+        domain + "/Activities/EarlyDetection/PhonologicalDyslexia/PhonologicalAwareness/1/indice.html",
+        domain + "/Activities/EarlyDetection/PhonologicalDyslexia/PhonologicalAwareness/2/indice.html",
+        domain + "/Activities/EarlyDetection/SuperficialDyslexia/VisualDiscrimination/1/indice.html"
+    ];
 
     return (
-        <div>
-            <iframe src={srcIframe}></iframe>
+        <div className='iframe-container'>
+            <iframe src={srcIframe[0]} title="Actividad" scrolling='no'></iframe>
         </div>
     )
 }
