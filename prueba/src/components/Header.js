31996a90e0d4d0e5f0309ee7d5d6a3dd033ac9eb
MariaJoseGG <mgiraldogu@unal.edu.co>
2023-10-14 19:05:58 -0500

    router

--- prueba/src/components/Header.js
+++ prueba/src/components/Header.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 import '../styles/Header.css'
 import logo from '../logo.png'
+import { NavLink, Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg header-container">
       <div className="container-fluid header-container">
-        <a className="navbar-brand" href="../index.js">
+        <NavLink className="navbar-brand">
           <img src={logo} alt="Logo" width="30" height="auto" className="d-inline-block align-text-top" />
-        </a>
-        <a className="navbar-brand" href="../index.js">Esmeralda</a>
+        </NavLink>
+        <Link className="navbar-brand">Esmeralda</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="../index.js">Inicio</a>
+              <NavLink className="nav-link active" aria-current="page">Inicio</NavLink>
             </li>
           </ul>
         </div>
--- prueba/src/index.js
+++ prueba/src/index.js
@@ -1,56 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Test } from './components/Test';
-import { Header } from './components/Header';
+import { App } from './components/App';
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-
-    {/* Props indicate the type of test (phonological dyslexia or superficial dyslexia) */}
-    <Header />
-
-    <Test
-      module="/Activities/EarlyDetection/PhonologicalDyslexia"
-      
-      competence1="/PhonologicalAwareness"
-      nameActivityC1_1="Seleccionar una palabra de acuerdo a su representación gráfica"
-      nameActivityC1_2="Encontrar letras específicas en medio de las palabras"
-      nameActivityC1_3="Seleccionar en orden las sílabas del nombre de una imagen"
-      nameActivityC1_4="Digitar la cantidad de sílabas que tiene una palabra"
-      nameActivityC1_5="Seleccionar la sílaba inicial de una palabra"
-
-      competence2="/LetterKnowledge"
-      nameActivityC2_1="Seleccionar en orden las letras de una palabra dada"
-      nameActivityC2_2="Seleccionar el audio que corresponde al sonido de una letra dada"
-      nameActivityC2_3="Seleccionar en orden alfabético un conjunto de letras dado"
-      nameActivityC2_4="Digitar la palabra que corresponde a la tarjeta dada del alfabeto"
-      nameActivityC2_5="Formar palabras a partir de un conjunto de letras dado"
-    />
-
-    <Test
-      module="/Activities/EarlyDetection/SuperficialDyslexia"
-
-      competence1="/VisualDiscrimination"
-      nameActivityC1_1=""
-      nameActivityC1_2=""
-      nameActivityC1_3=""
-      nameActivityC1_4=""
-      nameActivityC1_5=""
-
-      competence2="/Orthography"
-      nameActivityC2_1=""
-      nameActivityC2_2=""
-      nameActivityC2_3=""
-      nameActivityC2_4=""
-      nameActivityC2_5=""
-    />
-
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
