504dac1924c3c103d2e5bcb9cd26811856bcd048
PaulaTaborda7 <ptabordam@unal.edu.co>
2023-11-06 01:50:33 -0500

    Some general changes

--- prueba/src/pages/Esmeralda/HomeEsmeralda.jsx
+++ prueba/src/pages/Esmeralda/HomeEsmeralda.jsx
@@ -34,7 +34,13 @@ export const HomeEsmeralda = () => {
               </NavLink>
               <div className='card-body'>
                 <p className='card-text text-center textoEsmeralda'>
-                  "Es una dificultad específica de aprendizaje en la lectura; se caracteriza por dificultades en la conciencia fonológica, conocimiento de las letras y otras competencias. La conciencia fonológica se refiere a la habilidad de reconocer y manipular los sonidos del habla, como los fonemas, sílabas y segmentos de palabras. Las personas con dislexia fonológica suelen tener problemas para conectar los sonidos del habla con las letras que representan esos sonidos en palabras escritas, hacen una lectura visual de la palabra, es decir, observa la palabra de manera global, deduciendo (más que leyendo) las palabras conocidas, tienen problemas con la comprensión de lectura, les resulta imposible leer palabras que desconocen y pseudopalabras, ya que no pueden utilizar el mecanismo de conversión grafema a fonema, comenten errores en las palabras de sonido similar, entre otras características. Todo esto puede dificultar la lectura, en lugar de leer palabras de manera fluida, a menudo tienen que leerlas letra por letra o sílaba por sílaba". Citado de la tesis Esmeralda.
+                  Es una dificultad específica de aprendizaje en la lectura; se caracteriza por dificultades en la conciencia fonológica,
+                  conocimiento de las letras y otras competencias. La conciencia fonológica se refiere a la habilidad de reconocer y manipular
+                  los sonidos del habla, como los fonemas, sílabas y segmentos de palabras. Las personas con dislexia fonológica suelen tener problemas
+                  para conectar los sonidos del habla con las letras que representan esos sonidos en palabras escritas, hacen una lectura visual de la palabra,
+                  es decir, observa la palabra de manera global, deduciendo (más que leyendo) las palabras conocidas, tienen problemas con la comprensión de lectura,
+                  les resulta imposible leer palabras que desconocen. Todo esto puede dificultar la lectura, en lugar de leer palabras de manera fluida,
+                  a menudo tienen que leerlas letra por letra o sílaba por sílaba (Álvarez Palacio, 2015) - Tesis Esmeralda.
                 </p>
               </div>
             </div>
@@ -46,13 +52,30 @@ export const HomeEsmeralda = () => {
               </NavLink>
               <div className='card-body'>
                 <p className='card-text text-center textoEsmeralda'>
-                  "Es otro subtipo de dislexia que implica dificultades específicas en la lectura. El problema se relaciona con el reconocimiento de palabras enteras como unidades visuales. Las personas con este tipo de dislexia es frecuente que tengan dificultad para leer palabras cuya lectura y pronunciación no se correspondan. También comenten errores en las palabras complejas o largas y leen mejor las palabras regulares que las irregulares, tienen confusión de homófonos porque el acceso al léxico está guiado por el sonido y no por la ortografía de la palabra, suelen tener errores de omisión, adición o sustitución de letras. La dislexia superficial consiste en un mal funcionamiento de la ruta visual léxica o directa, o sea que el alumno no tiene problemas de lectura de palabras regulares, ya que no tiene una alteración de grafemas y fonemas, pero sí a la hora de leer palabras irregulares, donde se verá que comete omisiones, adiciones de letras o sustituciones de palabras". Citado de la tesis Esmeralda.
+                  Es otro subtipo de dislexia que implica dificultades específicas en la lectura.  Se caractetiza por
+                  dificultades en competencias como la discriminación visual y la ortografía. El problema se relaciona con el reconocimiento de palabras enteras como unidades visuales.
+                  Las personas con este tipo de dislexia es frecuente que tengan dificultad para leer palabras
+                  cuya lectura y pronunciación no se correspondan. También comenten errores en las palabras complejas
+                  o largas y leen mejor las palabras regulares que las irregulares, tienen confusión de homófonos
+                  porque el acceso al léxico está guiado por el sonido y no por la ortografía de la palabra,
+                  suelen tener errores de omisión, adición o sustitución de letras. La dislexia superficial
+                  consiste en un mal funcionamiento de la ruta visual léxica o directa, o sea que el alumno tiene problemas
+                  a la hora de leer palabras irregulares, donde se verá que comete omisiones, adiciones
+                  de letras o sustituciones de palabras (Álvarez Palacio, 2015) - Tesis Esmeralda.
                 </p>
               </div>
             </div>
           </div>
         </div>
       </div>
+
+      {/* ¿Video informativo para los estudiantes, ¿Te sientes identificado con la Dislexia? */}
+      {/* <div className="textoVideo"><br />
+        <h4>¿Qué es la Dislexia? ¿Te sientes identificado(a)?</h4>
+        <div className="video1">
+          <iframe width="853" height="480" src="https://www.youtube.com/embed/QJEXEIaOE4k" title="Paula Andrea Taborda Montes-Activador Creativo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        </div>
+      </div> */}
     </>
   )
 }
